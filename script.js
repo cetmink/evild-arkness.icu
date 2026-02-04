69cd4dc4c1a3a6c2bcf4defd4b858fb73b556c63
chisel0 <leon.prastalo@gmail.com>
2026-02-04 22:29:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const fog = document.getElementById("fog");
 
 let started = false;
 
-// footer link only
+// Footer link only
 fog.onclick = (e) => {
   e.stopPropagation();
   window.location.href = "https://6fog.lol";
 };
 
-// CLICK ANYWHERE TO START
+// Click anywhere to start
 document.body.onclick = async () => {
   if (started) return;
   started = true;
@@ -26,40 +26,36 @@ document.body.onclick = async () => {
 
   setInterval(() => {
     const now = new Date();
-    const time = now.toLocaleString("en-GB", { hour12: false });
+    const time = now.toLocaleString("en-GB", {
+      hour12: false
+    });
 
     const flag = data.country_code
       ? `https://flagcdn.com/w20/${data.country_code.toLowerCase()}.png`
       : "";
 
     info.innerHTML = `
-Welcome To Darkness
-------------------------
-TIME
-${time}
-
-DEVICE & BROWSER
+       Welcome To Darkness
+       ------- TIME -------
+      ${time}
+  ------- DEVICE & BROWSER -------
 ${navigator.platform} & ${navigator.userAgent.split(") ")[0]})
-
-IPDATA
-IP Address: ${data.ip}
-Country: ${data.country_name} ${flag ? `<img src="${flag}">` : ""}
-Location: ${data.city}, ${data.region}
-Provider: ${data.org}
-
-LOCATION
-Continent: ${data.continent_code}
-Postal Code: ${data.postal}
-Coordinates: ${data.latitude}, ${data.longitude}
-Timezone: ${data.timezone}
-Currency: ${data.currency}
-Language: ${navigator.language}
-
-SECURITY
-Tor: No
-Proxy: No
-------------------------
-END
+      ------- IPDATA -------
+    IP Address: ${data.ip}
+     Country: ${data.country_name} ${flag ? `<img src="${flag}">` : ""}
+      Location: ${data.city}, ${data.region}
+  Provider: ${data.org}
+     ------- LOCATION -------
+     Continent: ${data.continent_code}
+    Postal Code: ${data.postal}
+   Coordinates: ${data.latitude}, ${data.longitude}
+    Timezone: ${data.timezone}
+      Currency: ${data.currency}
+       Language: ${navigator.language}
+     ------- SECURITY -------
+            Tor: No
+          Proxy: No
+      ---------END----------
 `;
   }, 1000);
 };
